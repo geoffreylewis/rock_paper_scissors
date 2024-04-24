eba80b5c03b118b92aaba575ad789b522f378612
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-24 17:08:57 -0500

    Add files via upload
    Program is just getting started...

---
+++ 100_Days_Day_4.py
@@ -0,0 +1,9 @@
+##########################
+# Rock, Paper, Scissors! #
+##########################
+
+# Beginning of Game
+print("Let's play \"Rock, Paper, Scissors!\"  You ready?\n")
+
+# End of Game
+print("You win!")
